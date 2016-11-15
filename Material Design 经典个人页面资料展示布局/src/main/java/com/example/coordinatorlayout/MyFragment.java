94e6da19b4ba18d3577fb7d5bfbee04702eadb19
“SickoOrange” <181536584@qq.com>
2016-11-15 20:43:36 +0100

    9.commit: Android M 权限处理 解决了当用户拒绝权限请求同时点击了“不在询问” 选项后 如何友善的引导用户去设置页面 手动添加权限

---
+++ Material Design 经典个人页面资料展示布局/src/main/java/com/example/coordinatorlayout/MyFragment.java
@@ -0,0 +1,75 @@
+package com.example.coordinatorlayout;
+
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Orange on 2016/11/14.
+ */
+
+public class MyFragment extends BaseFragment {
+    private View view;
+    private List<String> mDatas;
+
+    @Override
+    public View initView(LayoutInflater inflater, @Nullable ViewGroup container) {
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_layout, container, false);
+            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            initData();
+            recyclerView.setAdapter(new MyAdapter());
+            return view;
+        }
+        return view;
+    }
+
+    protected void initData() {
+        mDatas = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            mDatas.add("Recycler View Item"+i);
+        }
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder viewHolder = new MyViewHolder(LayoutInflater.from(getContext()).inflate
+                    (R.layout.item, parent,
+                            false));
+            return viewHolder;
+        }
+
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.tv.setText(mDatas.get(position));
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tv = (TextView) itemView.findViewById(R.id.tx);
+        }
+    }
+
+}
